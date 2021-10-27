657b0ee6e5baa338f5d1c4df2484b60ce6f9844a
Melvin Davis <melvinodsa@gmail.com>
2021-10-27 19:15:04 +0530

    Updated helm

--- k8s/helm.sh
+++ k8s/helm.sh
@@ -1,5 +1,8 @@
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm install -f k8s/user-service/service-monitor.yaml dezerv-monitoring prometheus-community/kube-prometheus-stack
+helm repo add elastic https://helm.elastic.co
 helm install elasticsearch elastic/elasticsearch
 helm install kibana elastic/kibana
+helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
 helm install dezerv-tracing-op jaegertracing/jaeger-operator
 helm install -f jeager.yaml dezerv-tracing jaegertracing/jaeger
