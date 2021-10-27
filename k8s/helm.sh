9461ac6f3768049fc0cad0a3526a36bf2d3fa3b2
Melvin Davis <melvinodsa@gmail.com>
2021-10-27 20:31:21 +0530

    Updated helm

--- k8s/helm.sh
+++ k8s/helm.sh
@@ -5,4 +5,4 @@ helm install elasticsearch elastic/elasticsearch
 helm install kibana elastic/kibana
 helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
 helm install dezerv-tracing-op jaegertracing/jaeger-operator
-helm install -f jeager.yaml dezerv-tracing jaegertracing/jaeger
+helm install -f k8s/jeager/jeager.yaml dezerv-tracing jaegertracing/jaeger
